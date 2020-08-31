0766367353ce0183a5c973eee59e7e14ced6d351
Danilo Filgueira <danilo@origemmotos.com.br>
2020-08-31 13:58:36 -0300

    feat: add GET api to create a single notion record

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ from flask import request
 
 app = Flask(__name__)
 
-
 def createNotionTask(token, collectionURL, content):
     # notion
     client = NotionClient(token)
     cv = client.get_collection_view(collectionURL)
     row = cv.collection.add_row()
     row.title = content
-
 
 @app.route('/create_todo', methods=['GET'])
 def create_todo():
@@ -25,6 +23,25 @@ def create_todo():
     createNotionTask(token_v2, url, todo)
     return f'added {todo} to Notion'
 
+def createNotionRecord(token, collectionURL, content):
+    # notion
+    client = NotionClient(token)
+    cv = client.get_collection_view(collectionURL)
+    row = cv.collection.add_row()
+    row.name = content["name"]
+    row.field1 = content["field1"]
+
+@app.route('/create_record', methods=['GET'])
+def create_record():
+
+    content = {
+        "name": request.args.get('name'),
+        "field1": request.args.get('field1')
+    }
+    token_v2 = os.environ.get("TOKEN")
+    url = os.environ.get("URL")
+    createNotionTask(token_v2, url, content)
+    return f'added {content} to Notion'
 
 if __name__ == '__main__':
     app.debug = True
