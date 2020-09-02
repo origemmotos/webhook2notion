b1ee7ded1d9a345bc729e9b8bfd83a99eedc7f93
Danilo Filgueira <danilo@origemmotos.com.br>
2020-09-02 18:25:22 -0300

    feat: retrieve Item relation from collection; fill item column as relation

--- app.py
+++ app.py
@@ -23,23 +23,35 @@ def create_todo():
     createNotionTask(token_v2, url, todo)
     return f'added {todo} to Notion'
 
-def createNotionRecord(token, collectionURL, content):
+def getItemRelation(token, collectionURL, itemName):
+    cv = client.get_collection_view(collectionURL)
+    items = cv.collection.get_rows(search=itemName)
+    # notion-py search/query is not working, searching for item within items list
+    item = [i for i in items if i.Part == itemName]
+    return item
+
+def createNotionRecord(token, collectionURL, urlItems, content):
     # notion
     client = NotionClient(token)
     cv = client.get_collection_view(collectionURL)
+    # retrieve item from items collection
+    item = getItemRelation(token, urlItems, content["item"])
+    if (item == None):
+	print("Item '{}' not found in related database".format(content["item"]))
+	return -1
     row = cv.collection.add_row()
     row.nome = content["name"]
     row.action = content["action_type"]
-    row.item = content["item"]
+    row.item = item
     row.pn = content["part_number"]
     row.quantidade = content["quantity"]
     row.reason = content["reason"]
     row.obs = content["ps"]
     row.ts = content["ts"]
+    return 0
 
 @app.route('/create_record', methods=['GET'])
 def create_record():
-
     content = {
         "name": request.args.get('name'),
         "action_type": request.args.get('action_type'),
@@ -52,8 +64,12 @@ def create_record():
     }
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
-    createNotionRecord(token_v2, url, content)
-    return f'added {content} to Notion'
+    urlItems = os.environ.get("URL_ITEMS")
+    r = createNotionRecord(token_v2, url, urlItems, content)
+    if (r == 0):
+        return f'added {content} to Notion'
+    else:
+	return f'failed to add {content} to Notion'
 
 if __name__ == '__main__':
     app.debug = True
